b262ab180d01bf0ae04a4b353cf4ba1816eab4c3
Eelco Aartsen <eelco@aesset.nl>
2020-12-01 13:08:41 +0100

    Shows contracts on a very basic screen

--- rm/admin.py
+++ rm/admin.py
@@ -1,28 +1,29 @@
 from django.contrib import admin
 from rm.models import Contract, InterfaceCall
 
-
-
+# -------------------------------------------------------------------
+# ADMIN Interface page with Contracts
+# -------------------------------------------------------------------
 class ContractInline(admin.TabularInline):
     model = Contract
-
 
 class InterfaceCallAdmin(admin.ModelAdmin):
     """
     Change the way the list of interface calls looks
     """
-    inlines = [ ContractInline, ]
+    inlines = [ContractInline, ]
     list_display = ["date_time_creation", "status", "filename"]
-
 
 admin.site.register(InterfaceCall, InterfaceCallAdmin)
 
 
+# -------------------------------------------------------------------
+# ADMIN Contract page
+# -------------------------------------------------------------------
 class ContractAdmin(admin.ModelAdmin):
     """
     Change the way the list of contracts looks
     """
     list_display = ["nr", "description", "owner"]
-
 
 admin.site.register(Contract, ContractAdmin)
--- rm/views.py
+++ rm/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
 
-# Create your views here.
+from rm.models import Contract
+
+
+class ContractListView(ListView):
+    model = Contract
+    context_object_name = 'contract_list'
+    template_name = 'rm/contract_list.html'
