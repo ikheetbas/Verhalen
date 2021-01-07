4896ce29c643b0b8af43ce88608022bf8d83af35
Eelco Aartsen <eelco@aesset.nl>
2021-01-07 13:34:34 +0100

    Set a (not te be used) default for SECRET_KEY in to test if this fixes the failure of 'manage.py collectstatic' during the build

--- config/settings.py
+++ config/settings.py
@@ -24,7 +24,7 @@ ENVIRONMENT = os.environ.get('ENVIRONMENT', default='prod')
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.environ.get('SECRET_KEY')
+SECRET_KEY = os.environ.get('SECRET_KEY', default='Not_To_Be_Used_Default_Secret_Key')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.environ.get('DEBUG', default=False)
