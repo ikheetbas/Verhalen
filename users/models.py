a92efd5889645c9c34068b35c5a4e3c7a6074356
Eelco Aartsen <eelco@aesset.nl>
2021-01-25 10:05:58 +0100

    RM-107 Organisatie gegevens Technologie dep. in de database

--- users/models.py
+++ users/models.py
@@ -3,13 +3,21 @@ from django.db import models
 
 
 class Department(models.Model):
+    """
+    The classes Department, Cluster and Team are candidates for inheritance as subclasses of Organizational_unit.
+    But the possible constructions in Django didn't deliver that much benefits.
+    Therefore was decided to make them just plain classes, with the benefit of readability and easy relations and screen
+    generation for the admin screens.
+    """
     name = models.CharField("Naam", max_length=50)
+    cost_centre = models.CharField("Kostenplaats", max_length=50, default="<<onbekend>>")
     def __str__(self):
         return f"Department: {self.name}"
 
 
 class Cluster(models.Model):
     name = models.CharField("Naam", max_length=50)
+    cost_centre = models.CharField("Kostenplaats", max_length=50, default="<<onbekend>>")
     department = models.ForeignKey(Department, on_delete=models.CASCADE)
 
     def __str__(self):
@@ -18,6 +26,7 @@ class Cluster(models.Model):
 
 class Team(models.Model):
     name = models.CharField("Naam", max_length=50)
+    cost_centre = models.CharField("Kostenplaats", max_length=50, default="<<onbekend>>")
     cluster = models.ForeignKey(Cluster,on_delete=models.CASCADE)
 
     def __str__(self):
@@ -25,6 +34,9 @@ class Team(models.Model):
 
 
 class CustomUser(AbstractUser):
+    """
+    Custom User extending the Django AbstractUser gives us the possibility to add attributes and relations
+    """
     name_in_negometrix = models.CharField("Naam in Negometrix", max_length=150, blank=True, null=True)
 
     # Django Generally, ManyToManyField instances should go in the object that’s going to be edited on a form.
